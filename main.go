06f2eb69f939cfda69654eb22b9e1bd3b53c69d6
msarar <mohammedsarar7@gmail.com>
2026-01-08 14:00:10 +0100

    groupie tracker

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", handlers.HomeHandler)
+	http.HandleFunc("/artist",handlers.ArtistHandler)
 	fmt.Println("Starting server on http://localhost:8080")
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
