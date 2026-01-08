01fcf3769f92753b3e1c2438ee2c8d73a15b9934
msarar <mohammedsarar7@gmail.com>
2026-01-08 11:52:05 +0100

    logic layer

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"groupie-tracker/handlers"
+)
+
+func main() {
+	http.HandleFunc("/", handlers.HomeHandler)
+	fmt.Println("Starting server on http://localhost:8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
